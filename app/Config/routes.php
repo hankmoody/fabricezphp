e3dc9d994b7fa4072328d427fc6a3eb63e65aba7
Vikrant Gupta <vikrant1011@gmail.com>
2013-02-19 11:08:26 +0530

    fix routing

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -26,6 +26,7 @@
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
 	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
+	
 	Router::connect ('/app/webroot/', array('controller'=>'collections', 'action'=>'index'));
 /**
  * ...and connect the rest of 'Pages' controller's urls.
